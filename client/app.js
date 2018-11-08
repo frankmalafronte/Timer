be548ac63b60f07fd1acdbc4010e91df96a70db2
Frank <frankamalafronte@gmail.com>
2018-11-07 20:26:57 -0500

    added first draft of stopwatch component

--- client/app.js
+++ client/app.js
@@ -1,15 +1,17 @@
 import React from 'react'
-
 import {Navbar} from './components'
 import Routes from './routes'
+import Stopwatch from './components/stopwatch'
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Stopwatch</h1>
+        <Stopwatch />
+      </div>
+    )
+  }
 }
 
 export default App
